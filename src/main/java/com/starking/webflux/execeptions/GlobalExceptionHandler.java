f691b84ceaeb39c1ee5b3595cab51cee91ba7b90
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-19 20:58:45 -0300

    implementando o trace da minha maneira

--- src/main/java/com/starking/webflux/execeptions/GlobalExceptionHandler.java
+++ src/main/java/com/starking/webflux/execeptions/GlobalExceptionHandler.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -43,12 +45,26 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
 	}
 	
 	private Mono<ServerResponse> formatErrorResponse(ServerRequest request) {
-		Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+		String query = request.uri().getQuery();
+		ErrorAttributeOptions errorAttributeOptions = isErrorAttribute(query);
+		
+		Map<String, Object> errorAttributes = getErrorAttributes(request, errorAttributeOptions);
 		int status =(int) Optional.ofNullable(errorAttributes.get("status")).orElse(500);
 		
 		return ServerResponse.status(status)
 				.contentType(MediaType.APPLICATION_JSON)
 				.body(BodyInserters.fromValue(errorAttributes));
+	}
+
+	private ErrorAttributeOptions isErrorAttribute(String query) {
+		ErrorAttributeOptions errorAttributeOptions = isTraceEnabled(query)
+				? ErrorAttributeOptions.of(Include.STACK_TRACE)
+				: ErrorAttributeOptions.defaults();
+		return errorAttributeOptions;
+	}
+	
+	private boolean isTraceEnabled(String query) {
+		return !StringUtils.isEmpty(query) && query.contains("trace=true");
 	}
 
 }
