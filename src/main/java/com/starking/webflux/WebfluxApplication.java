b51e9e0e2994d93a329ac809cddcca449ebfc485
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-06-13 20:55:41 -0300

    comentando trecho com erro no codigo

--- src/main/java/com/starking/webflux/WebfluxApplication.java
+++ src/main/java/com/starking/webflux/WebfluxApplication.java
@@ -8,13 +8,13 @@ import reactor.blockhound.BlockHound;
 @SpringBootApplication
 public class WebfluxApplication {
 
-	static {
-		BlockHound.install(builder -> builder.allowBlockingCallsInside("java.util.UUID", "randomUUID")
-				.allowBlockingCallsInside("java.io.InputStream", "readNBytes")
-		.allowBlockingCallsInside("java.io.FilterInputStream", "read")
-		
-				);
-	}
+//	static {
+//		BlockHound.install(builder -> builder.allowBlockingCallsInside("java.util.UUID", "randomUUID")
+//				.allowBlockingCallsInside("java.io.InputStream", "readNBytes")
+//		.allowBlockingCallsInside("java.io.FilterInputStream", "read")
+//		
+//				);
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxApplication.class, args);
 	}
